3299debd9710bdd7720301d4509708814adac112
NDONG MBA Valentino wyhnel <110922220+yamitxc@users.noreply.github.com>
2024-03-17 20:28:00 +0000

    Delete MonServiceRPCInterface.java

--- MonServiceRPCInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MonServiceRPCInterface extends Remote {
-    int addition(int a, int b) throws RemoteException;
-    int multiplication(int a, int b) throws RemoteException;
-}
